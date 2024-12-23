3f6526f754a919e66665f227a38db0293cb1f7dd
Vasiliy Kurganov <wkurganow@gmail.com>
2024-12-24 03:39:54 +0700

    Первое решение для задачи 1

--- js/task1.js
+++ js/task1.js
@@ -1,1 +1,21 @@
-console.log(`fired!`);
+/**
+ * Напишите функцию, которая определяет уникальны ли все символы в строке.
+ * Регистр должен учитываться: 'a' и "А" разные символы
+ */
+
+function isUnique(string) {
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+
+    //abb
+    if (string.lastIndexOf(char) !== i) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isUnique("abcdef")); //true
+console.log(isUnique("1234567")); //true
+console.log(isUnique("abcABC")); //true
+console.log(isUnique("abcadef")); //false
